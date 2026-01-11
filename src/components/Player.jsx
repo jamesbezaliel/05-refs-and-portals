3740199e663faa7c7f8e091ca495fa6cab4e4c0e
jamesbezaliel <jamesjonathan343@gmail.com>
2026-01-11 16:00:47 +0700

    lesson 134, manage user input with two way binding (without using refs)

---
+++ src/components/Player.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Player() {
+  const [name, setName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setSubmitted(false);
+    setName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome {submitted && name !== "" ? name : "unknown entity"}</h2>
+      <p>
+        <input type="text" value={name} onChange={handleChange} />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
